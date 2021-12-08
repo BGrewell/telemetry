48f93d8ed0334733cce53dab2b7edcaa302ec3b6
Benjamin Grewell <bgrewell@gmail.com>
2021-12-07 20:38:27 -0800

    define protobufs

--- internal/timesync/timesync.go
+++ internal/timesync/timesync.go
@@ -3,8 +3,9 @@ package timesync
 import (
 	grpc "github.com/BGrewell/tgams/api/go"
 )
+
 func Offset(t1, t2, t3, t4 int64) int64 {
-    return ((t2 - t1) + (t3 - t4)) / 2
+	return ((t2 - t1) + (t3 - t4)) / 2
 }
 
 func Delay(t1, t2, t3, t4 int64) int64 {
@@ -12,9 +13,9 @@ func Delay(t1, t2, t3, t4 int64) int64 {
 }
 
 func CalcOffset(response *grpc.TimeSyncResponse) int64 {
-    return Offset(response.T1, response.T2, response.T3, response.T4)
+	return Offset(response.T1, response.T2, response.T3, response.T4)
 }
 
 func CalcDelay(response *grpc.TimeSyncResponse) int64 {
-    return Delay(response.T1, response.T2, response.T3, response.T4)
+	return Delay(response.T1, response.T2, response.T3, response.T4)
 }
